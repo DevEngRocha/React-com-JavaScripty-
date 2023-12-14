c51246e40ce021168e9fb824dfcdde2c54ffc8ed
Diego da Rocha <deveng.rocha@gmail.com>
2023-12-13 23:08:13 -0300

    Colocando os times

--- src/App.js
+++ src/App.js
@@ -6,6 +6,36 @@ import Time from './Componentes/Time';
 
 function App() {
 
+  const Times =[
+    {nome: 'Programação',
+    corPrimaria: '#D9F7E9',
+    corSecundaria: '#57C278'},
+
+    {nome: 'Front-End',
+    corPrimaria: '#E8F8FF',
+    corSecundaria: '#82CFFA'},
+
+    {nome: 'Data-Science',
+    corPrimaria: '#F0F8E2',
+    corSecundaria: '#A6D157'},
+
+    {nome: 'Devops',
+    corPrimaria: '#FDE7E8',
+    corSecundaria: '#E06B69'},
+
+    {nome: 'UX e Design',
+    corPrimaria: '#FAE9F5',
+    corSecundaria: '#DB6EBF'},
+
+    {nome: 'Mobile',
+    corPrimaria: '#FFF5D9',
+    corSecundaria: '#FFBA05'},
+
+    {nome: 'Inovação e Gestao',
+    corPrimaria: '#FFEEDF',
+    corSecundaria: '#FF8A29'},
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) =>{
@@ -17,13 +47,13 @@ function App() {
     <div className="App">
         <Banner />
         <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-        <Time nome="Programação"/>
-        <Time nome="Front-End"/>
-        <Time nome="Data Science"/>
-        <Time nome="Devops"/>
-        <Time nome="UX e Design"/>
-        <Time nome="Mobile"/>
-        <Time nome="Inovação e Gestão"/>
+        
+        {Times.map(time => <Time key={time.nome} nome ={time.nome} 
+        corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+        
+        
+        
+        
 
 
     </div>
--- src/Componentes/Time/index.js
+++ src/Componentes/Time/index.js
@@ -1,9 +1,12 @@
 import './Time.css'
 
 const Time = (props) =>{
+
+    const css = {backgroundColor:props.corPrimaria}
+    
     return(
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={css}>
+            <h3 style={{borderColor:props.corSecundaria}}>{props.nome}</h3>
         </section>
     )
 }
