caa006f659e0133f40e3d0cf1685edf66f5ce0d3
DIEGO ROCHA <deveng.rocha@gmail.com>
2023-12-19 23:12:34 -0300

    Mapeando os colaboradores

--- src/App.js
+++ src/App.js
@@ -6,54 +6,70 @@ import Time from './Componentes/Time';
 
 function App() {
 
-  const Times =[
-    {nome: 'Programação',
-    corPrimaria: '#D9F7E9',
-    corSecundaria: '#57C278'},
+  const Times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#D9F7E9',
+      corSecundaria: '#57C278'
+    },
 
-    {nome: 'Front-End',
-    corPrimaria: '#E8F8FF',
-    corSecundaria: '#82CFFA'},
+    {
+      nome: 'Front-End',
+      corPrimaria: '#E8F8FF',
+      corSecundaria: '#82CFFA'
+    },
 
-    {nome: 'Data-Science',
-    corPrimaria: '#F0F8E2',
-    corSecundaria: '#A6D157'},
+    {
+      nome: 'Data-Science',
+      corPrimaria: '#F0F8E2',
+      corSecundaria: '#A6D157'
+    },
 
-    {nome: 'Devops',
-    corPrimaria: '#FDE7E8',
-    corSecundaria: '#E06B69'},
+    {
+      nome: 'Devops',
+      corPrimaria: '#FDE7E8',
+      corSecundaria: '#E06B69'
+    },
 
-    {nome: 'UX e Design',
-    corPrimaria: '#FAE9F5',
-    corSecundaria: '#DB6EBF'},
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#FAE9F5',
+      corSecundaria: '#DB6EBF'
+    },
 
-    {nome: 'Mobile',
-    corPrimaria: '#FFF5D9',
-    corSecundaria: '#FFBA05'},
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFF5D9',
+      corSecundaria: '#FFBA05'
+    },
 
-    {nome: 'Inovação e Gestao',
-    corPrimaria: '#FFEEDF',
-    corSecundaria: '#FF8A29'},
+    {
+      nome: 'Inovação e Gestao',
+      corPrimaria: '#FFEEDF',
+      corSecundaria: '#FF8A29'
+    },
   ]
 
   const [colaboradores, setColaboradores] = useState([])
 
-  const aoNovoColaboradorAdicionado = (colaborador) =>{
+  const aoNovoColaboradorAdicionado = (colaborador) => {
     console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
   return (
     <div className="App">
-        <Banner />
-        <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-        
-        {Times.map(time => <Time key={time.nome} nome ={time.nome} 
-        corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
-        
-        
-        
-        
+      <Banner />
+      <Formulario times={Times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+
+      {Times.map(time => <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />)}
+
 
 
     </div>
--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -5,16 +5,7 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = (props) => {
-    const times = [
-        'Programacao',
-        'Front-End',
-        'Data Science ',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovacao e Gestao'
-    ]
-
+    
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -60,7 +51,7 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time"
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
